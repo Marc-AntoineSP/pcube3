01309b56100c1c3749445e595d5ec71053bcad11
MSKTArcanine <marcantoine1999@live.fr>
2025-11-12 20:47:46 +0100

    Vin

---
+++ src/main/java/fr/groupe/pcube/Fournisseur.java
@@ -0,0 +1,47 @@
+package fr.groupe.pcube;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fournisseur extends Personne {
+    private final Map<String, Vin> vins;
+
+    public Fournisseur(int id, String name, String email) {
+        super(id, name, email);
+        this.vins = new HashMap<>();
+    }
+
+    public Fournisseur(int id, String name, String email, Address address) {
+        super(id, name, email, address);
+        this.vins = new HashMap<>();
+    }
+
+    public Fournisseur(int id, String name, String email, Vin vin) {
+        super(id, name, email);
+        this.vins = new HashMap<>();
+        this.vins.put(vin.getName(), vin);
+    }
+
+    public Fournisseur(int id, String name, String email, Address address,
+            Vin vin) {
+        super(id, name, email, address);
+        this.vins = new HashMap<>();
+        this.vins.put(vin.getName(), vin);
+    }
+
+    public Vin getVin(String vin) throws IllegalArgumentException {
+        if (!this.vins.containsKey(vin)) {
+            throw new IllegalArgumentException(
+                    "Aucun vin ne correspond au vin demandé");
+        }
+        return this.vins.get(vin);
+    }
+
+    public boolean hasVin(String vin) {
+        return this.vins.containsKey(vin);
+    }
+
+    public void addVin(Vin vin) {
+        this.vins.put(vin.getName(), vin);
+    }
+}
