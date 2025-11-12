36d11db0ccb13b7b5cd72ea267372608c55d5980
MSKTArcanine <marcantoine1999@live.fr>
2025-11-12 20:05:10 +0100

    Address

---
+++ src/main/java/fr/groupe/pcube/Address.java
@@ -0,0 +1,59 @@
+package fr.groupe.pcube;
+
+public class Address {
+    private int codePostal;
+    private String rue;
+    private int numero;
+    private String ville;
+    private String pays;
+
+    public Address(int numero, String rue, String ville, int codePostal,
+            String pays) {
+        this.codePostal = codePostal;
+        this.rue = rue;
+        this.numero = numero;
+        this.ville = ville;
+        this.pays = pays;
+    }
+
+    public int getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(int codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+}
