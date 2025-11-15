0f89cba050f3d7951f4f87a7a39ff99e64b9d9e5
Marc-AntoineSP <marcantoine.saintpierre@viacesi.fr>
2025-11-15 22:51:33 +0100

    Client n'a pas besoin de connexion : pas de password

--- src/main/java/fr/groupe/pcube/Client.java
+++ src/main/java/fr/groupe/pcube/Client.java
@@ -1,47 +1,20 @@
 package fr.groupe.pcube;
 
 public class Client extends Personne {
-    private final String password;
 
-    public Client(int id, String name, String email, String password) {
-        super(id, name, email);
-        this.password = password;
-    }
-
-    public Client(int id, String name, String email, Address address,
-            String password) {
+    public Client(int id, String name, String email, Address address) {
         super(id, name, email, address);
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + ((password == null) ? 0 : password.hashCode());
-        return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Client other = (Client) obj;
-        if (password == null) {
-            if (other.password != null)
-                return false;
-        } else if (!password.equals(other.password))
-            return false;
-        return true;
+        return super.equals(obj);
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
 
 }
